86d3d256a66e424cd5cf45d2099ed0c58276be3c
sakib531 <mdsakibahammad531@gmail.com>
2025-09-13 22:56:36 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import SocialLogin from "./components/SocialLogin";
+import InputField from "./components/InputField";
+
+const App = () => {
+  return(
+    <div className="login-container">
+      <h2 className="form-title">Log in with</h2>
+
+      <SocialLogin />
+
+      <p className="separator"><span>or</span></p>
+
+      <form action="#" className="login-form">
+
+        <InputField type="email" placeholder="Email Address" icon="mail" />
+
+        <InputField type="password" placeholder="Password" icon="lock" />
+
+        <a href="#" className="forgot-pass-link">Forgot Password?</a>
+
+        <button className="login-button">Log In</button>
+
+      </form>
+
+      <p className="signup-text">Don&apos;t have an account? <a href="#">Signup now</a></p>
+
+    </div>
+  )
+}
+
+export default App;
+
